c96d5c0ea492f2fed12cfbcee0fb96df18976ebf
ikedahaj <132437214+ikedahaj@users.noreply.github.com>
2023-07-05 15:25:17 +0900

    hayaku

--- perio/perio.cpp
+++ perio/perio.cpp
@@ -7,7 +7,6 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
-#include "BM.h"
 
 #define Np          40000 // 4の倍数であること;NP=4*r^2*lo
 #define Nn          100
@@ -79,6 +78,31 @@ void usr_sincos(double kaku, double *x) { // x[0]がcos,x[1]issin;
     }
     x[0] = 1.0 - c * 0.5;
     x[1] = s;
+}
+double unif_rand(double left, double right) {
+    return left + (right - left) * rand() / RAND_MAX;
+}
+
+double gaussian_rand(void) {
+    static bool   iset = true;
+    static double gset;
+    double        fac, rsq, v1, v2;
+
+    if (iset) {
+        do {
+            v1 = unif_rand(-1, 1);
+            v2 = unif_rand(-1, 1);
+            rsq = v1 * v1 + v2 * v2;
+        } while (rsq >= 1.0 || rsq == 0.0);
+        fac = sqrt(-2.0 * log(rsq) / rsq);
+
+        gset = v1 * fac;
+        iset = false;
+        return v2 * fac;
+    } else {
+        iset = true;
+        return gset;
+    }
 }
 
 void ini_hex(double (*x)[dim2]) {
