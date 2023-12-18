efdc9fe218c131268cc0ef5c926eac0d362d12a8
ikedahaj <132437214+ikedahaj@users.noreply.github.com>
2023-12-18 20:53:46 +0900

    前修正

--- gaybane/gay_bane_force_two_circle.hpp
+++ gaybane/gay_bane_force_two_circle.hpp
@@ -51,10 +51,7 @@ constexpr double nth_root(double x, int t) {
     }
     return ans;
 }
-void ini_array(double (*x)[dim]) {
-    for (int i = 0; i < Np; ++i)
-        for (int j = 0; j < dim; ++j) x[i][j] = 0.0;
-}
+
 void ini_force(double (*f)[dim], double *f_theta) {
     for (int i = 0; i < Np; i++) {
         f[i][0] = 0;
@@ -279,7 +276,7 @@ inline void calc_force_wall_gay_bane(double *x, double *f, double theta,
         sa = center_left;
     x[0] -= sa;
     r2 = x[0] * x[0] + x[1] * x[1];
-    if (r2 < cutwf2) {
+    if (r2 > cutwf2) {
         double r, sini, cosi, dr_dot_et, dt_dr_dot_dt, bun_alpha, sigma, xi;
         r = sqrt(r2);
         sini = sin(theta);
@@ -287,17 +284,17 @@ inline void calc_force_wall_gay_bane(double *x, double *f, double theta,
         dr_dot_et = x[0] * cosi + x[1] * sini;
         dt_dr_dot_dt = x[0] * sini - x[1] * cosi;
         bun_alpha = 1. / (r2 - gay_alpha2 * dt_dr_dot_dt * dt_dr_dot_dt);
-        sigma = 1. / sqrt(1 - gay_alpha * (dr_dot_et * dr_dot_et * bun_alpha));
-        xi = r - sigma + 1.;
+        sigma = 1. / sqrt(1 - gay_alpha * dr_dot_et * dr_dot_et * bun_alpha);
+        xi = R + 0.5 - r - sigma + 1.;
         if (xi < cut) {
-            double sigma3, bun_alpha_p, r_1, r2_1, epsilon13, xi_1, xi2, xi6,
-                UWCA, dUWCA, dx_xi, dy_xi, dt_xi, epsilon1, epsilon1_N_1,
+            double sigma3, bun_alpha_p, r_1, r2_1, epsilon13, xi_1, xi2,
+                xi6, UWCA, dUWCA, dx_xi, dy_xi, dt_xi, epsilon1, epsilon1_N_1,
                 dx_epsilon1, dy_epsilon1, dt_epsilon1, epsilon2, epsilon2_M_1,
                 dr_epsilon2, dt_epsilon2;
             sigma3 = sigma * sigma * sigma;
             bun_alpha_p =
                 1. / (r2 - gay_alpha_p2 * dt_dr_dot_dt * dt_dr_dot_dt);
-            r_1 = 1. / r;
+            r_1 = -1. / r;
             r2_1 = r_1 * r_1;
             xi_1 = 1. / xi;
             xi2 = xi_1 * xi_1;
@@ -310,23 +307,23 @@ inline void calc_force_wall_gay_bane(double *x, double *f, double theta,
             dy_xi = r_1 * x[1] - x[0] * sigma3 * gay_alpha * (1 - gay_alpha2) *
                                      dr_dot_et * dt_dr_dot_dt * bun_alpha *
                                      bun_alpha;
-            dt_xi = gay_alpha * sigma3 * (1 - gay_alpha2) * r2 * dr_dot_et *
-                    dt_dr_dot_dt * bun_alpha * bun_alpha;
-            epsilon1 = 1 - gay_alpha2 * r2_1 * dt_dr_dot_dt * dt_dr_dot_dt;
+            dt_xi = gay_alpha * sigma3 * r2*(1-gay_alpha2) *
+                    dr_dot_et * dt_dr_dot_dt * bun_alpha * bun_alpha;
+            epsilon1 =sqrt(bun_alpha)*r_1;
             epsilon1_N_1 = fast_power(epsilon1, gay_N - 1);
             epsilon13 = epsilon1 * epsilon1 * epsilon1;
             dx_epsilon1 = epsilon13 * gay_alpha2 * r2_1 *
                           (sini - x[0] * r2_1 * dt_dr_dot_dt) * dt_dr_dot_dt;
             dy_epsilon1 = -epsilon13 * gay_alpha2 * r2_1 *
                           (cosi + x[1] * r2_1 * dt_dr_dot_dt) * dt_dr_dot_dt;
-            dt_epsilon1 = -epsilon13 * gay_alpha2 * dt_dr_dot_dt * dr_dot_et;
+            dt_epsilon1 = -epsilon13 * gay_alpha2 * dt_dr_dot_dt * dr_dot_et*r2_1;
             epsilon2 = 1 - gay_alpha_p * dr_dot_et * dr_dot_et * bun_alpha_p;
             epsilon2_M_1 = fast_power(epsilon2, gay_M - 1);
-            dr_epsilon2 = gay_alpha_p * (1 - gay_alpha_p2) * dr_dot_et *
+            dr_epsilon2 = gay_alpha_p * (-1 - gay_alpha_p2) * dr_dot_et *
                           dt_dr_dot_dt * bun_alpha_p *
                           bun_alpha_p;  // xの時y,yの時-xをかける;
-            dt_epsilon2 = 2 * gay_alpha_p * r2 * dr_dot_et * dt_dr_dot_dt *
-                          (1 - gay_alpha_p2);
+            dt_epsilon2 = 2 * gay_alpha_p * dr_dot_et * dt_dr_dot_dt *
+                        r2*(1-gay_alpha_p2) * bun_alpha_p * bun_alpha_p;
             f[0] -= (dUWCA * dx_xi * epsilon1 * epsilon2 +
                      UWCA * (dx_epsilon1 * gay_N * epsilon2 +
                              x[1] * dr_epsilon2 * epsilon1 * gay_M)) *
@@ -335,6 +332,7 @@ inline void calc_force_wall_gay_bane(double *x, double *f, double theta,
                      UWCA * (dy_epsilon1 * gay_N * epsilon2 -
                              x[0] * dr_epsilon2 * epsilon1 * gay_M)) *
                     epsilon1_N_1 * epsilon2_M_1;
+            double t1=dUWCA * dt_xi * epsilon1 * epsilon2,t2=UWCA * dt_epsilon1 * gay_N * epsilon2,t3=dt_epsilon2 * epsilon1 * gay_M*UWCA;
             *f_theta -= (dUWCA * dt_xi * epsilon1 * epsilon2 +
                          UWCA * (dt_epsilon1 * gay_N * epsilon2 +
                                  dt_epsilon2 * epsilon1 * gay_M)) *
@@ -348,8 +346,8 @@ inline void calc_force_wall_gay_bane(double *x, double *f, double theta,
 /// @param f 粒子の力。同上;
 /// @param theta 粒子の角度.値私で良い;
 /// @param f_theta 確度にかかる力.(&f[i])などと渡すこと.
-inline void calc_force_wall_gay_bane_anill(double *x, double *f, double theta,
-                                     double *f_theta,double R_ini) {
+inline void calc_wall_gay_bane_anill(double *x, double *f, double theta,
+                                     double *f_theta, double R_ini) {
     constexpr double gay_alpha = (gay_kappa * gay_kappa - 1.) /
                                  (gay_kappa * gay_kappa + 1.),
                      gay_alpha_p = (nth_root(gay_kappa_p, gay_M) - 1) /
@@ -357,16 +355,17 @@ inline void calc_force_wall_gay_bane_anill(double *x, double *f, double theta,
                      
                      gay_alpha2 = gay_alpha * gay_alpha,
                      gay_alpha_p2 = gay_alpha_p * gay_alpha_p;
-    double r2, sa,cutwf2 =
-                         (R_ini + 1.5 - cut - 1. / sqrt(1 - gay_alpha)) *
-                         (R_ini + 1.5 - cut - 1. / sqrt(1 - gay_alpha));
+    double r2, sa,
+                     cutwf2 =
+                         (R_ini + 1.5 - cut - 1. / nth_root(1 - gay_alpha, 2)) *
+                         (R_ini + 1.5 - cut - 1. / nth_root(1 - gay_alpha, 2));
     if (x[0] > 0)
-        sa = center_rignt;
+        sa = R_ini*rbit_2;
     else
-        sa = center_left;
+        sa = -R_ini*rbit_2;
     x[0] -= sa;
     r2 = x[0] * x[0] + x[1] * x[1];
-    if (r2 < cutwf2) {
+    if (r2 > cutwf2) {
         double r, sini, cosi, dr_dot_et, dt_dr_dot_dt, bun_alpha, sigma, xi;
         r = sqrt(r2);
         sini = sin(theta);
@@ -374,17 +373,17 @@ inline void calc_force_wall_gay_bane_anill(double *x, double *f, double theta,
         dr_dot_et = x[0] * cosi + x[1] * sini;
         dt_dr_dot_dt = x[0] * sini - x[1] * cosi;
         bun_alpha = 1. / (r2 - gay_alpha2 * dt_dr_dot_dt * dt_dr_dot_dt);
-        sigma = 1. / sqrt(1 - gay_alpha * (dr_dot_et * dr_dot_et * bun_alpha));
-        xi = r - sigma + 1.;
+        sigma = 1. / sqrt(1 - gay_alpha * dr_dot_et * dr_dot_et * bun_alpha);
+        xi = R + 0.5 - r - sigma + 1.;
         if (xi < cut) {
-            double sigma3, bun_alpha_p, r_1, r2_1, epsilon13, xi_1, xi2, xi6,
-                UWCA, dUWCA, dx_xi, dy_xi, dt_xi, epsilon1, epsilon1_N_1,
+            double sigma3, bun_alpha_p, r_1, r2_1, epsilon13, xi_1, xi2,
+                xi6, UWCA, dUWCA, dx_xi, dy_xi, dt_xi, epsilon1, epsilon1_N_1,
                 dx_epsilon1, dy_epsilon1, dt_epsilon1, epsilon2, epsilon2_M_1,
                 dr_epsilon2, dt_epsilon2;
             sigma3 = sigma * sigma * sigma;
             bun_alpha_p =
                 1. / (r2 - gay_alpha_p2 * dt_dr_dot_dt * dt_dr_dot_dt);
-            r_1 = 1. / r;
+            r_1 = -1. / r;
             r2_1 = r_1 * r_1;
             xi_1 = 1. / xi;
             xi2 = xi_1 * xi_1;
@@ -397,23 +396,23 @@ inline void calc_force_wall_gay_bane_anill(double *x, double *f, double theta,
             dy_xi = r_1 * x[1] - x[0] * sigma3 * gay_alpha * (1 - gay_alpha2) *
                                      dr_dot_et * dt_dr_dot_dt * bun_alpha *
                                      bun_alpha;
-            dt_xi = gay_alpha * sigma3 * (1 - gay_alpha2) * r2 * dr_dot_et *
-                    dt_dr_dot_dt * bun_alpha * bun_alpha;
-            epsilon1 = 1 - gay_alpha2 * r2_1 * dt_dr_dot_dt * dt_dr_dot_dt;
+            dt_xi = gay_alpha * sigma3 * r2*(1-gay_alpha2) *
+                    dr_dot_et * dt_dr_dot_dt * bun_alpha * bun_alpha;
+            epsilon1 =sqrt(bun_alpha)*r_1;
             epsilon1_N_1 = fast_power(epsilon1, gay_N - 1);
             epsilon13 = epsilon1 * epsilon1 * epsilon1;
             dx_epsilon1 = epsilon13 * gay_alpha2 * r2_1 *
                           (sini - x[0] * r2_1 * dt_dr_dot_dt) * dt_dr_dot_dt;
             dy_epsilon1 = -epsilon13 * gay_alpha2 * r2_1 *
                           (cosi + x[1] * r2_1 * dt_dr_dot_dt) * dt_dr_dot_dt;
-            dt_epsilon1 = -epsilon13 * gay_alpha2 * dt_dr_dot_dt * dr_dot_et;
+            dt_epsilon1 = -epsilon13 * gay_alpha2 * dt_dr_dot_dt * dr_dot_et*r2_1;
             epsilon2 = 1 - gay_alpha_p * dr_dot_et * dr_dot_et * bun_alpha_p;
             epsilon2_M_1 = fast_power(epsilon2, gay_M - 1);
-            dr_epsilon2 = gay_alpha_p * (1 - gay_alpha_p2) * dr_dot_et *
+            dr_epsilon2 = gay_alpha_p * (-1 - gay_alpha_p2) * dr_dot_et *
                           dt_dr_dot_dt * bun_alpha_p *
                           bun_alpha_p;  // xの時y,yの時-xをかける;
-            dt_epsilon2 = 2 * gay_alpha_p * r2 * dr_dot_et * dt_dr_dot_dt *
-                          (1 - gay_alpha_p2);
+            dt_epsilon2 = 2 * gay_alpha_p * dr_dot_et * dt_dr_dot_dt *
+                        r2*(1-gay_alpha_p2) * bun_alpha_p * bun_alpha_p;
             f[0] -= (dUWCA * dx_xi * epsilon1 * epsilon2 +
                      UWCA * (dx_epsilon1 * gay_N * epsilon2 +
                              x[1] * dr_epsilon2 * epsilon1 * gay_M)) *
@@ -422,14 +421,14 @@ inline void calc_force_wall_gay_bane_anill(double *x, double *f, double theta,
                      UWCA * (dy_epsilon1 * gay_N * epsilon2 -
                              x[0] * dr_epsilon2 * epsilon1 * gay_M)) *
                     epsilon1_N_1 * epsilon2_M_1;
+            double t1=dUWCA * dt_xi * epsilon1 * epsilon2,t2=UWCA * dt_epsilon1 * gay_N * epsilon2,t3=dt_epsilon2 * epsilon1 * gay_M*UWCA;
             *f_theta -= (dUWCA * dt_xi * epsilon1 * epsilon2 +
                          UWCA * (dt_epsilon1 * gay_N * epsilon2 +
                                  dt_epsilon2 * epsilon1 * gay_M)) *
                         epsilon1_N_1 * epsilon2_M_1;
         }
     }
-    x[0] += sa;
-}
+    x[0] += sa;}
 void cell_list(int (*list)[Nn], double (*x)[dim]) {
     int              map_index, nx[dim];
     constexpr double threash2 =
